4b7ddb3e5caf3f01e98c7f92d44d2a80c3999609
Joyce Lee <joycelee901@gmail.com>
2021-05-21 12:26:25 -0500

    commit tree ea7877105d4546ffe2d154b349d3c0f58c1a7585

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let result
+  if (feet <= 400) {
+    result = 'This one is on me!';
+  } else if (feet > 2000 && feet < 2500) {
+      result = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+      result = 'No can do.';
+  } else {
+      result = null;
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  let result
+  if (city === 'NYC') {
+    result = 'Ok, sounds good.';
+  } else {
+    result = 'No go.'
+  }
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(howMuch){
+  let result
+  if (howMuch === 'generous') {
+    result = 'Thank you so much.';
+  } else if (howMuch === 'not as generous') {
+    result = 'Thank you.';
+  } else {
+    result = 'Bye.'
+  }
+  return result;
 }
